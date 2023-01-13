76aa93ec6353ff9002766e7497927a97c37d1bce
lhan <ling.han@canada.ca>
2023-01-13 15:39:08 -0500

    add commonly used stuff into the SharedModule

--- projects/master-file/src/app/shared/shared.module.ts
+++ projects/master-file/src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
-// import { NgModule } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgPipesModule } from '../pipes/pipes.module';
+import { TranslateModule } from '@ngx-translate/core';
 
-
-
-// @NgModule({
-//   exports: [
-//     CommonModule,
-//     FormsModule
-//   ]
-// })
-// export class SharedModule { }
+@NgModule({
+  exports: [
+    CommonModule,
+    FormsModule,
+    NgPipesModule,
+    TranslateModule
+  ]
+})
+export class SharedModule { }
