ee05258717843f1f8ddc2fa545a6bc71125c9c94
lhan <TravelBack@1>
2023-02-22 22:34:51 -0500

    cleanup

--- projects/master-file/src/app/common/common-feature.module.ts
+++ projects/master-file/src/app/common/common-feature.module.ts
@@ -2,26 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ExpanderComponent} from './expander/expander.component';
 import {BrowserModule} from '@angular/platform-browser';
-import {ReactiveFormsModule} from '@angular/forms';
-import {AppPrivacyComponent} from './privacy/privacy-statement-en.component';
-import {AppSecurityComponent} from './security/security-disclaimer-en.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    BrowserModule,
-    ReactiveFormsModule
-  ],
-  declarations: [
-    ExpanderComponent,
-    AppPrivacyComponent,
-    AppSecurityComponent
-  ],
-  exports: [
-    ExpanderComponent,
-    AppPrivacyComponent,
-    AppSecurityComponent
-  ]
+  imports: [CommonModule, BrowserModule, ReactiveFormsModule],
+  declarations: [ExpanderComponent],
+  exports: [ExpanderComponent],
 })
-export class CommonFeatureModule {
-}
+export class CommonFeatureModule {}
