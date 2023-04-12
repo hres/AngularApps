f3815c6c22a009f8d97f72d3f133ab563d7ddd37
lhan <TravelBack@1>
2023-04-12 15:37:22 -0400

    cleanup

--- projects/master-file/src/app/registration/registration.module.ts
+++ projects/master-file/src/app/registration/registration.module.ts
@@ -4,7 +4,6 @@ import { FileIoModule } from '../filereader/file-io/file-io.module';
 import { SharedModule } from '../shared/shared.module';
 import { MasterFileBaseComponent } from '../master-file-base/master-file-base.component';
 import { RegulatoryInformationComponent } from '../regulatory-information/regulatory-information.component';
-import { MasterFileDetailsComponent } from '../master-file-details/master-file.details.component';
 import { MasterFileFeeComponent } from '../master-file-fee/master-file.fee.component';
 import { AddressDetailsComponent } from '../address/address.details/address.details.component';
 import { DataService } from '../shared/data.service';
@@ -15,7 +14,6 @@ import { ContactDetailsComponent } from '../contact/contact.details/contact.deta
   declarations: [
     MasterFileBaseComponent,
     RegulatoryInformationComponent,
-    MasterFileDetailsComponent,
     MasterFileFeeComponent,
     AddressDetailsComponent,
     ContactDetailsComponent
@@ -24,7 +22,6 @@ import { ContactDetailsComponent } from '../contact/contact.details/contact.deta
   exports: [
     MasterFileBaseComponent,
     RegulatoryInformationComponent,
-    MasterFileDetailsComponent,
     MasterFileFeeComponent,
     AddressDetailsComponent,
     ContactDetailsComponent
