9c2e99d1f378e411c87feaf506e32ace4a07ba5c
Diana.Miraflor <diana.miraflor@hc-sc.gc.ca>
2023-06-16 13:07:23 -0400

    HPFBFORMS-248. Required label for postal code

--- projects/master-file/src/app/address/address.details/address.details.component.ts
+++ projects/master-file/src/app/address/address.details/address.details.component.ts
@@ -41,7 +41,6 @@ export class AddressDetailsComponent implements OnInit, OnChanges, AfterViewInit
   public provinces: Array<any> = [];
   public provStateList: Array<any> = [];
   public showProvText = true;
-  public showPostalRequired = false;
   public provinceLabel = 'addressDetails.province';
   public postalPattern: RegExp = null;
   public postalLabel = 'postal.canada';
@@ -174,8 +173,6 @@ export class AddressDetailsComponent implements OnInit, OnChanges, AfterViewInit
     if (this.showFieldErrors) {
       this.cdr.detectChanges(); // doing our own change detection
     }
-
-    this.showPostalRequired = true;
   }
 
 
