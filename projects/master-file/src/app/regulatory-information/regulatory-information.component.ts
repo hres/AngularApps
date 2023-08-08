b63b6e73d89b7e4ea061fd70d011ff11d24c2fe0
Diana.Miraflor <diana.miraflor@hc-sc.gc.ca>
2023-08-08 11:41:38 -0400

    HPFBFORMS-304. Removed Response to Proc. Request

--- projects/master-file/src/app/regulatory-information/regulatory-information.component.ts
+++ projects/master-file/src/app/regulatory-information/regulatory-information.component.ts
@@ -50,8 +50,8 @@ export class RegulatoryInformationComponent implements OnInit, OnDestroy {
   mfTypeTxDescSub!: Subscription;
   mfUseSub!: Subscription;
 
-  showDateAndRequesterTxDescs: string[] = ['12', '13', '14']; // Transaction Description values are defined in txDescriptions.json
-  showDateAndRequesterOnlyTxDescs: string[] = ['12', '14'];
+  showDateAndRequesterTxDescs: string[] = ['12', '13']; // Transaction Description values are defined in txDescriptions.json
+  showDateAndRequesterOnlyTxDescs: string[] = ['12', '13'];
   noFeeTxDescs: string[] = ['1', '3', '5', '8', '9', '12', '14', '20'];
 
   constructor(private _regulatoryInfoService: RegulatoryInformationService, private _fb: FormBuilder) {
