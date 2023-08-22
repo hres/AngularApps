6dbfdb37a9f511d91c3de540f9fcbca7c56d61e8
lhan <TravelBack@1>
2023-08-22 14:19:40 -0400

    add a util method to get form control's name

--- projects/master-file/src/app/globals/globals.service.ts
+++ projects/master-file/src/app/globals/globals.service.ts
@@ -307,6 +307,30 @@ export class GlobalsService {
     return [].concat(...arrays);
   }
   
+  public static getControlName (control: AbstractControl) {
+    var controlName = null;
+    var parent = control["_parent"];
+
+    // only such parent, which is FormGroup, has a dictionary 
+    // with control-names as a key and a form-control as a value
+    if (parent instanceof FormGroup)
+    {
+        // now we will iterate those keys (i.e. names of controls)
+        Object.keys(parent.controls).forEach((name) =>
+        {
+            // and compare the passed control and 
+            // a child control of a parent - with provided name (we iterate them all)
+            if (control === parent.controls[name])
+            {
+                // both are same: control passed to Validator
+                //  and this child - are the same references
+                controlName = name;
+            }
+        });
+    }
+    // we either found a name or simply return null
+    return controlName;
+  }
 
 }
 
@@ -329,3 +353,4 @@ export class DataMapping {
     this.outputDataType = outputDataType;
   }
 }
+
