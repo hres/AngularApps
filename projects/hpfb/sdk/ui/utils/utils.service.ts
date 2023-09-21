f7d5d54fa33ecbf32373ef11807dba5bf83fd0c2
lhan <TravelBack@1>
2023-09-21 16:10:24 -0400

    add util service method

--- projects/hpfb/sdk/ui/utils/utils.service.ts
+++ projects/hpfb/sdk/ui/utils/utils.service.ts
@@ -56,6 +56,10 @@ export class UtilsService {
     return lang === FRENCH;
   }
 
+  isFrench(lang: string): boolean {
+    return lang === FRENCH;
+  }
+
   getFormattedDate(format: string): string {
     const today = new Date();
     const pipe = new DatePipe('en-US');
