84953f71e4c1c8e20c3b93b03eb650007ce0452f
Diana.Miraflor <diana.miraflor@hc-sc.gc.ca>
2023-10-30 14:22:07 -0400

    REPMDFORM-55 bug fix

--- projects/md-co/src/app/app.constants.ts
+++ projects/md-co/src/app/app.constants.ts
@@ -3,16 +3,19 @@ export const DATA_PATH: string = './assets/data/';
 export const NEW:string='NEW';
 export const AMEND:string='AMEND';
 export const FINAL:string='FINAL';
+export const REVISE:string='REVISE';
+export const ACTIVE:string='ACTIVE';
+export const REMOVE:string='REMOVE';
 export const XSLT_PREFIX = 'REP_MDS_CO_';
 
 export const AMEND_OTHER_REASON_CODE = 'OTHER';   // this needs to match the OTHER code value in amendReasons.json
 
-export enum ContactStatus {
-    NEW,
-    REVISE,
-    REMOVE,
-    ACTIVE
-  } 
+export const ContactStatus = {
+  NEW,
+  REVISE,
+  REMOVE,
+  ACTIVE
+} 
 
 // export enum EnrollmentStatus {
 //     NEW = 'fr_NEW',
