03b2bb73ed4f51569feed5d09149d8d5dee09d1d
Diana.Miraflor <diana.miraflor@hc-sc.gc.ca>
2024-01-09 15:05:04 -0500

    Add MD AI

---
+++ projects/md-ai/src/app/app.constants.ts
@@ -0,0 +1,17 @@
+export const ROOT_TAG: string = 'DEVICE_TRANSACTION_ENROL';
+export const DATA_PATH: string = './assets/data/';
+
+export const XSLT_PREFIX = 'REP_MDS_RT_';
+
+// DIANA TODO - Change strings according to app's instructions
+export const helpInstructionHeadings = [
+  'loadFileIndx',
+  'compREPInx',
+  'rationaleInx',
+  'conStatInx',
+  'routIdInx',
+  'addRecordInx',
+  'desRenewalInx',
+  'desFinanceInx',
+  'licenseNumsInx',
+];
