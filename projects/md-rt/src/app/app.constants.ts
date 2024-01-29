097c3b323b9d56a80b194ce1bf33d2f02d915dda
lhan <TravelBack@1>
2024-01-29 11:10:00 -0500

    update message properties; update help indexes

--- projects/md-rt/src/app/app.constants.ts
+++ projects/md-rt/src/app/app.constants.ts
@@ -50,6 +50,7 @@ export enum AmendReason {
 export const helpInstructionHeadings = [
   'loadFileInx',
   'transactionInfoInx',
+  'dossierIdInx',
   'contactIdInx',
   'deviceClassInx',
   'applicationNumberInx',
