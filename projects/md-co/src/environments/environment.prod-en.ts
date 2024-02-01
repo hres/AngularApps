66204c4c300fe2c72062c0024adb42c483dc471d
lhan <TravelBack@1>
2024-02-01 12:57:19 -0500

    REPMDFORM-184 update the site index page in breadcrumbs for MD CO external

--- projects/md-co/src/environments/environment.prod-en.ts
+++ projects/md-co/src/environments/environment.prod-en.ts
@@ -1,9 +1,10 @@
 import {baseEnv as base} from './env';
 
+// environment setting for English external prod
 export const environment = {
   ...base,
   production: true,
-  serverBaseUrl: 'https://lam-dev.hres.ca/rep-dev/',
+  serverBaseUrl: 'https://lam-dev.hres.ca/REP-Form/',
   internal: false,
   lang: 'en',
 };
