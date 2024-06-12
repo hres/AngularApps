973ff2cff811c714f2f101d007df77c820b93e00
lhan <TravelBack@1>
2024-06-12 13:30:41 -0400

    update form base

--- projects/mf-rt/src/app/form-base/form-base.component.ts
+++ projects/mf-rt/src/app/form-base/form-base.component.ts
@@ -1,15 +1,15 @@
-import {Component, OnInit, Input, ViewEncapsulation, AfterViewInit, ChangeDetectorRef, ViewChild, HostListener } from '@angular/core';
+import {Component, OnInit, Input, ViewEncapsulation, AfterViewInit, ChangeDetectorRef, ViewChild, HostListener, ViewChildren, QueryList } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { FileConversionService, CheckSumService, UtilsService, ConverterService, VersionService, FileIoModule, ErrorModule, PipesModule, EntityBaseService, HelpIndex } from '@hpfb/sdk/ui';
+import { FileConversionService, CheckSumService, UtilsService, ConverterService, VersionService, FileIoModule, ErrorModule, PipesModule, EntityBaseService, HelpIndex, ControlMessagesComponent, ConvertResults } from '@hpfb/sdk/ui';
 import { GlobalService } from '../global/global.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { AppFormModule } from '../app.form.module';
 import { FilereaderInstructionComponent } from "../filereader-instruction/filereader-instruction.component";
-import { ROOT_TAG } from '../app.constants';
+import { MASTER_FILE_OUTPUT_PREFIX, ROOT_TAG } from '../app.constants';
 import { RegulatoryInformationComponent } from "../regulatory-information/regulatory-information.component";
 import { MasterFileBaseService } from './master-file-base.service';
-import { Ectd, Transaction, TransactionEnrol} from '../models/transaction';
+import { Ectd, FeeDetails, Transaction, TransactionEnrol} from '../models/transaction';
 
 @Component({
     selector: 'app-form-base',
@@ -21,41 +21,64 @@ import { Ectd, Transaction, TransactionEnrol} from '../models/transaction';
     imports: [CommonModule, TranslateModule, ReactiveFormsModule, FileIoModule, ErrorModule, PipesModule, AppFormModule, FilereaderInstructionComponent, RegulatoryInformationComponent]
 })
 export class FormBaseComponent implements OnInit, AfterViewInit {
-processFile($event: any) {
-throw new Error('Method not implemented.');
-}
   public errors;
-  @Input() lang;
-  @Input() helpTextSequences;
-
-  public helpIndex: HelpIndex; // todo CompanyBaseService.getHelpTextIndex();
-  public masterFileForm: FormGroup;
-  public rootTagText = ROOT_TAG; 
-  public showErrors: boolean;
-  public errorList = [];
-  public headingLevel = 'h2';
+  lang: string;
+  helpIndex: HelpIndex;
+  @ViewChildren(ControlMessagesComponent) msgList: QueryList<ControlMessagesComponent>;
 
   private _regulatoryInfoErrors = [];
-
+  private _transFeeErrors = [];
+  private _addressErrors = [];
+  private _contactErrors = [];
+  private _agentAddressErrors = [];
+  private _agentContactErrors = [];
+  private _baseErrors = [];
+  private _contactConfirmError = [];
+  private __certficationErrors = [];
+  public masterFileForm: FormGroup; // todo: do we need it? could remove?
+  public errorList = [];
+  public showErrors: boolean;
+  public showContactFees: boolean[];
+  public headingLevel = 'h2';
+  
+  public rootTagText = ROOT_TAG; 
   private appVersion: string;
   private xslName: string;
 
   public enrollModel : Transaction;
   public transactionEnrollModel: TransactionEnrol;
   public ectdModel: Ectd;
+  // public holderAddressModel = MasterFileBaseService.getEmptyAddressDetailsModel();
+  // public agentAddressModel = MasterFileBaseService.getEmptyAddressDetailsModel();
+  // public holderContactModel = this.transactionEnrollModel.contact_info.holder_contact;
+  // public agentContactModel = this.transactionEnrollModel.contact_info.agent_contact;
+  public transFeeModel: FeeDetails;
 
-  @ViewChild(RegulatoryInformationComponent) regulatoryInfoComponent: RegulatoryInformationComponent;
- 
-  constructor(private _baseService: MasterFileBaseService, private _globalService: GlobalService, private cdr: ChangeDetectorRef,
-    private fb: FormBuilder
+  public notApplicable: boolean = false;
+  public holder: string = 'holder';
+  public agent: string = 'agent';
+
+  showDateAndRequesterTxDescs: string[] = ['12', '14', '13'];
+  showDateAndRequesterOnlyTxDescs: string[] = ['12', '14'];
+  noFeeTxDescs: string[] = ['1', '3', '5', '8', '9', '12', '14', '20'];
+
+  constructor(
+    private _fb: FormBuilder,
+    private cdr: ChangeDetectorRef,
+    private  _baseService: MasterFileBaseService, private _globalService: GlobalService, private _utilsService: UtilsService,
+    private fileServices: FileConversionService
   ) {
+    this.showErrors = false;
+    this.showContactFees = [true, true];
+    this.appVersion = this._globalService.appVersion;
+    let xsltVersion = this.appVersion.split('.',2).join("_");
+    this.xslName = MASTER_FILE_OUTPUT_PREFIX.toUpperCase() + '_RT_' + xsltVersion + '.xsl';
+  }
 
-  }
-  
-  ngAfterViewInit(): void {
-    document.location.href = '#def-top';
-  }
-  ngOnInit(): void {
+  async ngOnInit() {
+    if (!this.masterFileForm) {
+      this.masterFileForm = this._baseService.getReactiveModel(this._fb);
+    }
     try {
 
       if (!this._globalService.enrollment) {
@@ -69,23 +92,66 @@ throw new Error('Method not implemented.');
 
       this.transactionEnrollModel = this.enrollModel[this.rootTagText];
 
+      this.lang = this._globalService.currLanguage;
       this.helpIndex = this._globalService.helpIndex;
-      this.masterFileForm = this.fb.group({}); 
-
     } catch (e) {
       console.error(e);
     }
   }
+  ngAfterViewInit(): void {
+    document.location.href = '#def-top';
 
-  public hideErrorSummary() {
-    return this.showErrors && this.errorList && this.errorList.length > 0;
+    this.msgList.changes.subscribe(errorObjs => {
+      let temp = [];
+      this._updateErrorList(errorObjs);
+      this.processErrors();
+    });
+    this.msgList.notifyOnChanges();
   }
 
+  private _updateErrorList(errorObjs) {
+    let certifTempErrors = [];
+    let contactConfirmTempError = [];
+    if (errorObjs) {
+      errorObjs.forEach(
+        error => {
+          // console.log(error);   
+          if (error.label === 'contactInfoConfirm') {
+            contactConfirmTempError.push(error);
+          } else {
+            certifTempErrors.push(error);
+          }
+        }
+      );
+    }
+    
+    this._contactConfirmError = contactConfirmTempError;
+    this.__certficationErrors = certifTempErrors;
+  }
+
+
   processErrors() {
-    // console.log('@@@@@@@@@@@@ processErrors');
+    // console.log('@@@@@@@@@@@@ Processing errors in ApplicationInfo base compo
     this.errorList = [];
-    // concat the error arrays
+
     this.errorList = this.errorList.concat(this._regulatoryInfoErrors);
+
+    if (this.showContactFees[0] === true) {
+      this.errorList = this.errorList.concat(
+        this._addressErrors.concat(this._contactErrors)
+      );
+      if(!this.notApplicable)
+        this.errorList = this.errorList.concat(
+          this._agentAddressErrors.concat(this._agentContactErrors)
+        );
+      this.errorList = this.errorList.concat(this._contactConfirmError);
+    }
+
+    if (this.showContactFees[1] === true) {
+      this.errorList = this.errorList.concat(this._transFeeErrors);
+    }
+
+    this.errorList = this.errorList.concat(this.__certficationErrors);
 
     this.cdr.detectChanges(); // doing our own change detection
   }
@@ -95,8 +161,92 @@ throw new Error('Method not implemented.');
     this.processErrors();
   }
 
-  public setShowContactFeesFlag(flag) {   //ling todo in signal
-    // this.showContactFees = flag;
+  // processTransFeeErrors(errorList) {
+  //   this._transFeeErrors = errorList;
+  //   this.processErrors();
+  // }
+
+  // processAddressErrors(errorList) {
+  //   this._addressErrors = errorList;
+  //   this.processErrors();
+  // }
+
+  // processContactErrors(errorList) {
+  //   this._contactErrors = errorList;
+  //   this.processErrors();
+  // }
+
+  // processAgentAddressErrors(errorList) {
+  //   this._agentAddressErrors = errorList;
+  //   this.processErrors();
+  // }
+
+  // processAgentContactErrors(errorList) {
+  //   this._agentContactErrors = errorList;
+  //   this.processErrors();
+  // }
+
+  public hideErrorSummary() {
+    return this.showErrors && this.errorList && this.errorList.length > 0;
+  }
+
+  public saveXmlFile() {
+    this.showErrors = true;
+    this.processErrors();
+    this._saveXML();  
+  }
+
+  public saveWorkingCopyFile() {
+    const result = this._prepareForSaving(false);
+    const fileName = this._generateFileName();
+    this.fileServices.saveJsonToFile(result, fileName, null);
+  }
+
+  public processFile(fileData: ConvertResults) {
+    console.log('processing file.....');
+    console.log(fileData);
+    this.transactionEnrollModel = fileData.data.TRANSACTION_ENROL;
+    this.ectdModel = this.transactionEnrollModel.ectd;
+    
+    if (this.ectdModel.lifecycle_record.sequence_description_value) {
+      this.showContactFees[0] = !this.showDateAndRequesterOnlyTxDescs.includes(
+        this.ectdModel?.lifecycle_record.sequence_description_value._id);
+      this.showContactFees[1] = !this.noFeeTxDescs.includes(
+        this.ectdModel?.lifecycle_record.sequence_description_value._id);
+    }
+    // if (this.showContactFees[0] === true) {
+    //   this.holderAddressModel = fileData.data.TRANSACTION_ENROL.contact_info.holder_name_address;
+    //   this.holderContactModel = fileData.data.TRANSACTION_ENROL.contact_info.holder_contact;
+    //   this.agentAddressModel = fileData.data.TRANSACTION_ENROL.contact_info.agent_name_address;
+    //   this.agentContactModel = fileData.data.TRANSACTION_ENROL.contact_info.agent_contact;
+    // }
+    // if (this.showContactFees[1] === true) {
+      this.transFeeModel = fileData.data.TRANSACTION_ENROL.fee_details;
+    // }
+
+    // MasterFileBaseService.mapDataModelToFormModel(this.transactionEnrollModel, this.masterFileForm);
+    this.agentInfoOnChange();
+  }
+
+  private _updateSavedDate() {
+    const today = new Date();
+    const pipe = new DatePipe('en-US');
+    this.transactionEnrollModel.date_saved = pipe.transform(
+      today,
+      'yyyy-MM-dd-hhmm'
+    );
+  }
+
+  private _updateSoftwareVersion() {
+    this.transactionEnrollModel.software_version = this.appVersion;
+  }
+
+  public preload() {
+    // console.log("Calling preload")
+  }
+
+  public setShowContactFeesFlag(flag) {
+    this.showContactFees = flag;
 
     // if (this.showContactFees[0] === false) {
     //   this.holderAddressModel = MasterFileBaseService.getEmptyAddressDetailsModel();
@@ -112,7 +262,7 @@ throw new Error('Method not implemented.');
     //   this.transFeeModel = MasterFileBaseService.getEmptyMasterFileFeeModel();
     //   this._transFeeErrors = [];
     // }
-    // this.processErrors();
+    this.processErrors();
   }
 
 
@@ -120,24 +270,100 @@ throw new Error('Method not implemented.');
   unloadNotification($event: any) {
     $event.returnValue = true;
   }
-  
-  public saveXmlFile() {
-	  this.showErrors = true;
-    this.processErrors();
 
-    if (this.errorList && this.errorList.length > 0) {
-      document.location.href = '#topErrorSummary';
-    } else {
-      // const result: Enrollment = this._prepareForSaving(true);
-      // const fileName: string = this._buildfileName(result);
-      // const xslName: string = XSLT_PREFIX.toUpperCase() + this._versionService.getApplicationMajorVersionWithUnderscore(this._globalService.$appVersion) + '.xsl';
-      // this._fileService.saveXmlToFile(result, fileName, true, xslName);
+  _saveXML() {
+    if (this.errorList && this.errorList.length < 1) {
+      const result = this._prepareForSaving(true);
+      const fileName = this._generateFileName();
+      
+      console.log('save ...');
+      this.fileServices.saveXmlToFile(result, fileName, true, this.xslName);
+      return;
+    }
+    document.location.href = '#topErrorSummaryId';
+  }
+
+  private _prepareForSaving(finalFile: boolean): Transaction {
+    if (finalFile) {
+      // this.masterFileModel.enrol_version = this.masterFileModel.enrol_version; // todo do we need the enrol_version field?
+    }
+
+    this._updateSavedDate();
+    this._updateSoftwareVersion();
+    
+    if (this.ectdModel.lifecycle_record.sequence_description_value) {
+      this.showContactFees[0] = !this.showDateAndRequesterOnlyTxDescs.includes(
+        this.ectdModel?.lifecycle_record.sequence_description_value._id);
+      this.showContactFees[1] = !this.noFeeTxDescs.includes(
+        this.ectdModel?.lifecycle_record.sequence_description_value._id);
+    }
+    // if (this.showContactFees[0] === true) {
+    //   this.transactionEnrollModel.contact_info = MasterFileBaseService.getEmptyContactInfo();
+    //   this.transactionEnrollModel.contact_info.holder_name_address = this.holderAddressModel;
+    //   this.transactionEnrollModel.contact_info.holder_contact = this.holderContactModel;
+    //   this.transactionEnrollModel.contact_info.agent_name_address = this.agentAddressModel;
+    //   this.transactionEnrollModel.contact_info.agent_contact = this.agentContactModel;
+    //   this.transactionEnrollModel.contact_info.contact_info_confirm =
+    //     this.masterFileForm.controls['contactInfoConfirm'].value;
+    //   this.transactionEnrollModel.contact_info.agent_not_applicable = this.notApplicable;
+    // } else {
+    //   this.transactionEnrollModel.contact_info = null;
+    // }
+    // if (this.showContactFees[1] === true) {
+    //   this.transactionEnrollModel.fee_details = this.transFeeModel;
+    // } else {
+    //   this.transactionEnrollModel.fee_details = null;
+    // }
+
+    this.transactionEnrollModel.certify_accurate_complete =
+      this.masterFileForm.controls['certifyAccurateComplete'].value;
+    this.transactionEnrollModel.full_name =
+      this.masterFileForm.controls['fullName'].value;
+    this.transactionEnrollModel.submit_date =
+      this.masterFileForm.controls['submitDate'].value;
+    this.transactionEnrollModel.consent_privacy = 
+      this.masterFileForm.controls['consentPrivacy'].value;
+
+
+    const result: Transaction = {
+      TRANSACTION_ENROL: this.transactionEnrollModel,
+    };
+
+    console.log('_prepareForSaving ~ result', JSON.stringify(result));
+
+    return result;
+  }
+
+  private _generateFileName(): string {
+    let fileName =
+      MASTER_FILE_OUTPUT_PREFIX + "-" + 
+      this.transactionEnrollModel.ectd.dossier_id +
+      '-' +
+      this.transactionEnrollModel.date_saved;
+    return fileName;
+  }
+
+  public agentInfoOnChange() {
+    this.notApplicable = this.masterFileForm.controls['notApplicable'].value;
+    // console.log ("this.notApplicable=",this.notApplicable, typeof this.notApplicable);
+
+    // if (this.notApplicable) {
+    //   this.agentAddressModel = MasterFileBaseService.getEmptyAddressDetailsModel();
+    //   this.agentContactModel = MasterFileBaseService.getEmptyContactModel();
+    //   this._agentAddressErrors = null;
+    //   this._agentContactErrors = null;
+    // }
+
+    this.processErrors();
+  }
+  
+  public onChanged(e, controlName) {
+    if (e?.target?.checked === false) {
+      this.masterFileForm.controls[controlName].reset();
     }
   }
 
-  public saveWorkingCopyFile() {
-    // const result: Enrollment = this._prepareForSaving(false);
-    // const fileName: string = this._buildfileName(result);
-    // this._fileService.saveJsonToFile(result, fileName, null);
-  }
+  checkDateValidity(event: any): void {
+    this._utilsService.checkInputValidity(event, this.masterFileForm.get('submitDate'), 'invalidDate');
+  }  
 }
