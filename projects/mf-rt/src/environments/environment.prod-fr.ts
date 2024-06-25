c57c149381cf439e0b809c805367947dab106586
BAucoinHC <156108457+BAucoinHC@users.noreply.github.com>
2024-06-25 14:48:00 -0400

    breadcrumbs fix

--- projects/mf-rt/src/environments/environment.prod-fr.ts
+++ projects/mf-rt/src/environments/environment.prod-fr.ts
@@ -3,6 +3,6 @@ import {baseEnv as base} from './env';
 export const environment = {
   ...base,
   production: true,
-  serverBaseUrl: 'https://lam-dev.hres.ca/REP-Form/',
+  serverBaseUrl: 'https://health-products.canada.ca/',
   lang: 'fr',
 };
