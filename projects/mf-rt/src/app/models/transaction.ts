19467aed6c20f1688ab6371b7a5055b604a14cb5
lhan <TravelBack@1>
2024-06-25 14:53:27 -0400

    change who_responsible_fee from string to IIdTextLabel

--- projects/mf-rt/src/app/models/transaction.ts
+++ projects/mf-rt/src/app/models/transaction.ts
@@ -57,7 +57,7 @@ export interface LifecycleRecord {
 export interface FeeDetails {
   are_there_access_letters: string;
   number_of_access_letters: string;
-  who_responsible_fee: string;
+  who_responsible_fee: IIdTextLabel;
   account_number: string;
   cra_business_number: string
 }
