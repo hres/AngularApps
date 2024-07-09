265ae458d355801c983e1b00cca9a5c2a65113ce
lhan <TravelBack@1>
2024-07-09 11:41:43 -0400

    add a function to get app major version number

--- projects/hpfb/sdk/ui/version/version.service.ts
+++ projects/hpfb/sdk/ui/version/version.service.ts
@@ -7,22 +7,32 @@ export class VersionService {
   }
 
   /**
-   * get the major version number in the application version
+   * get the major and minor version of the application concatenated with '.'
    * @param appVersion format is 1.0.0
-   * @returns string, major version of the application
+   * @returns string
    */
   getApplicationMajorVersion(appVersion: string): string {
     const majorVersion = appVersion.split('.', 2).join('.');
     return majorVersion;
   }
 
-    /**
-   * get the major version number in the application version using underscore as delimiter
+   /**
+   * get the major and minor version of the application concatenated with '_'
    * @param appVersion format is 1.0.0
-   * @returns string, major version of the application
+   * @returns string
    */
-    getApplicationMajorVersionWithUnderscore(appVersion: string): string {
-      const majorVersion = appVersion.split('.', 2).join('_');
-      return majorVersion;
-    }
+  getApplicationMajorVersionWithUnderscore(appVersion: string): string {
+    const majorVersion = appVersion.split('.', 2).join('_');
+    return majorVersion;
+  }
+
+/**
+ * Extracts the major version number from a version string.
+ *
+ * @param versionStr - The version string in the format "major.minor.patch", e.g., "1.0.0".
+ * @returns The major version number as an integer.
+ */
+  getMajorVersion(versionStr: string): number {
+    return parseInt(versionStr.split('.')[0], 10);
+  }
 }
