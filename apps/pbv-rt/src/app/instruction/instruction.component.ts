93c7ddfbdb91eefe5f895a592441edfffd4ad83d
lhan <TravelBack@1>
2024-10-28 14:33:29 -0400

    pharmabio rt, REPPBFORM-725, REPPBFORM-720 control number

--- apps/pbv-rt/src/app/instruction/instruction.component.ts
+++ apps/pbv-rt/src/app/instruction/instruction.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { HelpTextModuleModule } from './help-text-module.module';
 
 @Component({
   selector: 'app-instruction',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, HelpTextModuleModule],
   templateUrl: './instruction.component.html',
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./instruction.component.css']
