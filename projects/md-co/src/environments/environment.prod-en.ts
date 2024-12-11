06591c2026943a86b6c3fc559df02d81c27ebb23
Diana.Miraflor <diana.miraflor@hc-sc.gc.ca>
2024-12-11 08:29:01 -0500

    Changed serverBaseUrl + mds_index to mds-index

--- projects/md-co/src/environments/environment.prod-en.ts
+++ projects/md-co/src/environments/environment.prod-en.ts
@@ -4,7 +4,7 @@ import {baseEnv as base} from './env';
 export const environment = {
   ...base,
   production: true,
-  serverBaseUrl: 'https://lam-dev.hres.ca/REP-Form/',
+  serverBaseUrl: 'https://lam-dev.hres.ca/rep-dev/',
   internal: false,
   lang: 'en',
 };
