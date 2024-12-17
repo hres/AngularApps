6b7827bd18467e6cd2aa3281e790e3283065b511
dmiraflorhc <diana.miraflor@hc-sc.gc.ca>
2024-12-17 14:31:32 -0500

    Removed sdk version number in change-dependency.py

--- python/utils/change-dependency.py
+++ python/utils/change-dependency.py
@@ -7,7 +7,7 @@ dist_path = os.path.join('dist','hpfb',library_name)
 package_json_path = os.path.join(dist_path, 'package.json')
 # The dependency to modify (you can change this)
 dependency_name = '@hpfb/sdk'  # Replace with the actual dependency name
-new_dependency_path = 'file:../../libs/hpfb-sdk-1.1.0.tgz'  # Replace with your new dependency path
+new_dependency_path = ''  
 # Check if the package.json exists
 if os.path.exists(package_json_path):
     # Open the package.json file
@@ -15,7 +15,16 @@ if os.path.exists(package_json_path):
         package_json = json.load(file)
     
     # Modify the dependency in the package.json file
-    if 'dependencies' in package_json:
+    if 'dependencies' in package_json and dependency_name in package_json['dependencies']:
+        old_dependency_path = package_json['dependencies'][dependency_name]
+        print(f"Old dependency path for {dependency_name}: {old_dependency_path}")
+
+        if old_dependency_path.startswith('file:../../../'):
+            # Replace "file:../../../" with "file:../../"
+            new_dependency_path = old_dependency_path.replace('file:../../../', 'file:../../')
+            print(f"Updated dependency path: {new_dependency_path}")
+        else:
+            print("Current dependency path does not begin with 'file:../../../'")
         package_json['dependencies'][dependency_name] = new_dependency_path
     else:
         package_json['dependencies'] = {dependency_name: new_dependency_path}
