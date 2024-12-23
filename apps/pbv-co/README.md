399fc39abc262413f928a4607e552da0eb3121f5
Andrea McKay <andrea.mckay@hc-sc.gc.ca>
2024-12-23 08:23:26 -0500

    Minor changes to titles for pi and co forms

--- apps/pbv-co/README.md
+++ apps/pbv-co/README.md
@@ -1,4 +1,4 @@
-# pharmabio-rt
+# pharmabio-co
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.2.3.
 
--- apps/pbv-pi/src/app/app.component.spec.ts
+++ apps/pbv-pi/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'pharmabio-rt' title`, () => {
+  it(`should have the 'pharmabio-pi' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pharmabio-rt');
+    expect(app.title).toEqual('pharmabio-pi');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pharmabio-rt');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pharmabio-pi');
   });
 });
