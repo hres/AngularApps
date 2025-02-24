a6cf31706aef78c813576c813a3dfb0f23721e0b
DDoucetHC <danielle.doucet@hc-sc.gc.ca>
2025-02-24 16:40:36 -0500

    always show NOC instructions

--- apps/csp/src/app/app.component.ts
+++ apps/csp/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { environment } from '../environments/environment';
 import { Title } from '@angular/platform-browser';
 import { ENGLISH, VersionService, InstructionService } from '@hpfb/sdk/ui';
 import { GlobalService } from './global/global.service';
-import { HELP_FOOTNOTE_PREFIX, HELP_FOOTNOTE_SUFFIX, helpInstructionHeadings, helpInstructionHeadingsInternal } from './app.constants';
+import { HELP_FOOTNOTE_PREFIX, HELP_FOOTNOTE_SUFFIX, helpInstructionHeadings } from './app.constants';
 import { ContainerComponent } from './container/container.component';
 
 @Component({
@@ -35,12 +35,7 @@ export class AppComponent {
     translate.use(this.language)
     this._globalService.currLanguage = this.language;
     this.isInternal = environment.isInternal;
-
-    if(this.isInternal === false){
-      this._globalService.helpIndex = this._instructionService.createHelpSequence(HELP_FOOTNOTE_PREFIX, HELP_FOOTNOTE_SUFFIX, helpInstructionHeadings);
-    } else if(this.isInternal === true){
-      this._globalService.helpIndex = this._instructionService.createHelpSequence(HELP_FOOTNOTE_PREFIX, HELP_FOOTNOTE_SUFFIX, helpInstructionHeadingsInternal);
-    }
+    this._globalService.helpIndex = this._instructionService.createHelpSequence(HELP_FOOTNOTE_PREFIX, HELP_FOOTNOTE_SUFFIX, helpInstructionHeadings);
 
     this._globalService.appVersion = this._versionService.getApplicationVersion(environment);
     this._globalService.devEnv = !environment.production;
