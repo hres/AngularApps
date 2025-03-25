688d80948383659d7615924e54b8d651dc3df2ad
DDoucetHC <danielle.doucet@hc-sc.gc.ca>
2025-03-25 13:57:58 -0400

    co - moved amend message up

--- apps/pbv-co/src/app/filereader-instruction/filereader-instruction.component.ts
+++ apps/pbv-co/src/app/filereader-instruction/filereader-instruction.component.ts
@@ -1,19 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, ViewEncapsulation, OnInit } from '@angular/core';
 import { GlobalService } from '../global/global.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-filereader-instruction',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './filereader-instruction.component.html',
   styles: ``,
   encapsulation: ViewEncapsulation.None
 })
 export class FilereaderInstructionComponent implements OnInit {
   @Input() lang: string;
+  @Input() isInternal: boolean;
+  @Input() isStatusFinal: boolean;
 
-  isInternal: boolean;
   public showFieldErrors: boolean = false;
 
   constructor(
