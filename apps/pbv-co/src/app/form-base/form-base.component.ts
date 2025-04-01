db0223ce0231df3bee36b9495adb726bec820713
DDoucetHC <danielle.doucet@hc-sc.gc.ca>
2025-04-01 12:01:29 -0400

    fixed based off comments

--- apps/pbv-co/src/app/form-base/form-base.component.ts
+++ apps/pbv-co/src/app/form-base/form-base.component.ts
@@ -307,17 +307,19 @@ export class FormBaseComponent implements OnInit, AfterViewInit {
     document.location.href = '#topErrorSummaryId';
   }
 
-  public isAmend(){
-    const appType = String(this.companyEnrolModel.application_type);
-    
-    if(this.companyEnrolModel.application_type._id === ENROLMENT_STATUS.FINAL){
+  public isAmend() {    
+    if (this.companyEnrolModel.application_type._id === ENROLMENT_STATUS.FINAL) {
       return (!this.isInternal && this.isStatusFinal);
-    } else if(this.companyEnrolModel.software_version < this._globalService.appVersion && appType === ENROLMENT_STATUS.FINAL) {
-      return (!this.isInternal && this.isStatusFinal);
-    } else{
-      return undefined;
+    } else if (this.companyEnrolModel.software_version < this._globalService.appVersion) {
+      const appType = String(this.companyEnrolModel.application_type);
+      if (appType === ENROLMENT_STATUS.FINAL) {
+        return (!this.isInternal && this.isStatusFinal);
+      } 
+      return false;
     }
+    return false;
   }
+  
 
   private _prepareForSaving(xmlFile: boolean): Company {
     let contactsFormArrayValue = null;
