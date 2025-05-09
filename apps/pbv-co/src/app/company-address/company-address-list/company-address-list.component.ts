177e9b82d60205636265c164c3af5f6cd8bb650c
dmiraflorhc <diana.miraflor@hc-sc.gc.ca>
2025-05-09 09:12:06 -0400

    CO - ART-7978 + XSLT changes

--- apps/pbv-co/src/app/company-address/company-address-list/company-address-list.component.ts
+++ apps/pbv-co/src/app/company-address/company-address-list/company-address-list.component.ts
@@ -99,43 +99,47 @@ export class CompanyAddressListComponent extends BaseListComponent<AddressRecord
     this.companyRolesOptionList = updatedRoles;
   }
 
-  handleRemoveRoleError(event : any) {
-    // event: unchecked role
-    const recordId = event.id;
-    const role = event.role;
-    const roleIndex = event.roleIndex;
-    let id = null;
+  /**
+   * Deprecated
+   * @param event
+   */
+  // handleRemoveRoleError(event : any) {
+  //   // event: unchecked role
+  //   const recordId = event.id;
+  //   const role = event.role;
+  //   const roleIndex = event.roleIndex;
+  //   let id = null;
 
-    // Check if there are any other records with the same role that's been unchecked,
-    // If so, clear the errors
+  //   // Check if there are any other records with the same role that's been unchecked,
+  //   // If so, clear the errors
 
-    // Look for other records that has the same role as the role that has been unchecked
-    const currentRolesArray = this._signalService.getSelectedAddressCompanyRoles()();
-    for (const item of currentRolesArray) {
-      const idMatch = item.match(/^(\d+)/); // Extract the number (prefix)
-      const itemRole = item.replace(/^\d+/, ''); // Extract role type
+  //   // Look for other records that has the same role as the role that has been unchecked
+  //   const currentRolesArray = this._signalService.getSelectedAddressCompanyRoles()();
+  //   for (const item of currentRolesArray) {
+  //     const idMatch = item.match(/^(\d+)/); // Extract the number (prefix)
+  //     const itemRole = item.replace(/^\d+/, ''); // Extract role type
       
-      if (itemRole === role && idMatch !== recordId) {
-        id = Number(idMatch?.[1]); // Return the number as a number type
-        break;
-      }
-    }
+  //     if (itemRole === role && idMatch !== recordId) {
+  //       id = Number(idMatch?.[1]); // Return the number as a number type
+  //       break;
+  //     }
+  //   }
 
-    // If id has been found, find FormGroup with matching recordId. Set role's errors to null
-    if (id) {
-      const formGroupWithId = this.recordFormArray.controls.find(
-        (group) => group.get('recordId')?.value === id
-      ) as FormGroup | undefined;
+  //   // If id has been found, find FormGroup with matching recordId. Set role's errors to null
+  //   if (id) {
+  //     const formGroupWithId = this.recordFormArray.controls.find(
+  //       (group) => group.get('recordId')?.value === id
+  //     ) as FormGroup | undefined;
         
-      if (formGroupWithId) {
-        let addressRoles = formGroupWithId.get('addressInfo.addressCompanyRoles') as FormArray;
-        const roleControl = addressRoles.at(roleIndex);
-        if (roleControl.errors) {
-          roleControl.setErrors(null);
-        }
-      }
-    }
-  }
+  //     if (formGroupWithId) {
+  //       let addressRoles = formGroupWithId.get('addressInfo.addressCompanyRoles') as FormArray;
+  //       const roleControl = addressRoles.at(roleIndex);
+  //       if (roleControl.errors) {
+  //         roleControl.setErrors(null);
+  //       }
+  //     }
+  //   }
+  // }
 
   saveAddressRecord(event: any): void {
     const index = event.index;
