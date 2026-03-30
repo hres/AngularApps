ecd188ed286df0856a50a6817989ea586cdc2603
Bianca Andreescu <bianca.andreescu@hc-sc.gc.ca>
2026-03-30 15:00:39 -0400

    PI - Jira 1029, 1030 add warning texts

--- apps/pbv-pi/src/app/filereader-instruction/filereader-instruction.component.ts
+++ apps/pbv-pi/src/app/filereader-instruction/filereader-instruction.component.ts
@@ -1,14 +1,33 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, Input, ViewEncapsulation, OnInit, ChangeDetectorRef, SimpleChanges, OnChanges } from '@angular/core';
+import { GlobalService } from '../global/global.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-filereader-instruction',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './filereader-instruction.component.html',
   styles: ``,
   encapsulation: ViewEncapsulation.None
 })
-export class FilereaderInstructionComponent {
+export class FilereaderInstructionComponent implements OnChanges {
   @Input() lang: string;
+  @Input() isInternal: boolean;
+  @Input() isStatusFinal: boolean;
+
+  alertVisible = false;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    const showAlert = this.isStatusFinal && !this.isInternal;
+
+    if (showAlert && !this.alertVisible) {
+      setTimeout(() => {
+        this.alertVisible = true;
+        this.cdr.detectChanges(); // 👈 tells Angular to re-render
+      }, 400);
+    }
+  }
 }
