6c8d2858b748e5bb6bc68a3ada36394ad0600fc8
tfang54321 <fangwen88@yahoo>
2026-08-22 11:22:13 -0400

    fix key issue when shared subject for delete or discard address or contact trigger unrelated subscribere

--- projects/hpfb/sdk/ui/record/record-action-service/record-discard.service.ts
+++ projects/hpfb/sdk/ui/record/record-action-service/record-discard.service.ts
@@ -9,4 +9,22 @@ export class RecordDiscardService {
   confirmDiscard(index: number) {
     this.discardConfirmedSubject.next(index);
   }
+
+
+
+  //create different subjects for address and contact discard confirmation to avoid trigger different subscriber when using same subject as above. this is really biger potential issue in current code in many places. so we need to create different subjects for address and contact discard confirmation.
+
+  private discardAddressConfirmedSubject = new Subject<number>();
+  discardAddressConfirmed$ = this.discardAddressConfirmedSubject.asObservable();
+
+  confirmDiscardAddress(index: number) {
+    this.discardAddressConfirmedSubject.next(index);
+  }
+
+  private discardContactConfirmedSubject = new Subject<number>();
+  discardContactConfirmed$ = this.discardContactConfirmedSubject.asObservable();
+
+  confirmDiscardContact(index: number) {
+    this.discardContactConfirmedSubject.next(index);
+  }
 }
